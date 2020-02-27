f0d28a535e9374ca93493acccbad1b124f7552ff
JacekWy <34398043+JacekWy@users.noreply.github.com>
2020-02-27 09:56:06 +0100

    new update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 //Magazyn
-var food = 100;
-var wood = 150;
-var stone = 300;
+var food = parseInt(localStorage.getItem("Food"));
+var wood = parseInt(localStorage.getItem("Wood"));
+var stone = parseInt(localStorage.getItem("Stone"));
+
+
+//Lvl production
+var foodprodLvl = [25,50,125,150,180,210,250,312];
+var woodprodLvl = [50,150,225,350,480,510,650,712];
+var stoneprodLvl = [70,180,255,350,410,480,650,712];
+
+//Lvl Budynku
+var foodbLvl = localStorage.getItem("foodblvl");
+var woodbLvl = localStorage.getItem("woodblvl");
+var stonebLvl = localStorage.getItem("stoneblvl");
+
 
 //Produkcja na minute
-var foodProd = 25;
-var woodProd = 40;
-var stoneProd = 50;
+var foodProd = foodprodLvl[foodbLvl];
+var woodProd = woodprodLvl[woodbLvl];
+var stoneProd = stoneprodLvl[stonebLvl];
 
 
 
 function Update_resources(){
     let count = 0;
+
     //Produkcja update
     $(".data-container-food").html("Produkcja: " + foodProd + "/min");
     $(".data-container-wood").html("Produkcja: " + woodProd + "/min");
@@ -23,13 +36,49 @@ function Update_resources(){
     $(".stone").html("Stone: " + stone);
 
 
+    //Dodawanie co 10s surowcow
     setInterval(function(){
         count++;
         console.log(count);
         
-        $(".food").html("Food:" + (food += foodProd));
+        $(".food").html("Food: " + (food += foodProd));
         $(".wood").html("Wood: " + (wood += woodProd));
         $(".stone").html("Stone: " + (stone += stoneProd));
 
-    },100000)
+        localStorage.setItem("Food",food);
+        localStorage.setItem("Wood",wood);
+        localStorage.setItem("Stone",stone);
+
+        
+
+        
+        if(food > 1000){
+            $(".update-btn-food").show(".update-btn-food");
+        }else{
+            $(".update-btn-food").hide(".update-btn-food");
+        }
+
+        if(wood > 1800){
+            $(".update-btn-wood").show(".update-btn-wood");
+        }else{
+            $(".update-btn-wood").hide(".update-btn-wood");
+        }
+
+    },2000)
+
 }
+
+
+$("#updateFood").click(function () { 
+    food-= 1000;
+    console.log(food);
+    localStorage.setItem("Food",food);
+    foodbLvl++
+    localStorage.setItem("foodblvl",foodbLvl);
+});
+
+$("#updateWood").click(function () { 
+    wood-= 1800;
+    console.log(wood);
+    localStorage.setItem("Wood",wood); 
+});
